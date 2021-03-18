38a62d0f28c06af379b8f0aa1de51ae7b1a84e05
Alexander Elian <alex.elian14@outlook.com>
2021-03-18 22:59:35 +0000

    inc. RSI(2) MeanRev Strat

--- strategies/strat.R
+++ strategies/strat.R
@@ -14,27 +14,45 @@ getOrders <- function(store, newRowList, currentPos, info, params) {
   
   if (store$iter > params$lookback) {
     startIndex <-  store$iter - params$lookback
+    #for (i in 1:1) {  #One Series
     for (i in 1:length(params$series)) {
 
-      x <- MomentumStrategy(store, newRowList, currentPos, info, params, i, startIndex)
-      
-      ##same for function mean reversion
-      
+      #x <- MomentumStrategy(store, newRowList, currentPos, info, params, i, startIndex)
+      x <- MeanRevStrategy(store, newRowList, currentPos, info, params, i, startIndex) #(2)
+      #list <- MarketMakingStrategy(store, newRowList, currentPos, info, params, i, startIndex)
+      #print(x)
       if (x != 0){
         pos[params$series[i]] <- x
       }
-      
     }
   }
+  #For market Orders
   marketOrders <- pos
+
+  #For Limit Orders (Market Making)
+  #limitOrders1 <- pos
+  limitOrders1 <- allzero
+  limitPrices1 <- allzero
+  limitOrders2 <- allzero
+  limitPrices2 <- allzero
   
+  #print(marketOrders)
   return(list(store=store,marketOrders=marketOrders,
-              limitOrders1=allzero,limitPrices1=allzero,
-              limitOrders2=allzero,limitPrices2=allzero))
+              limitOrders1=limitOrders1,limitPrices1=limitPrices1,
+              limitOrders2=limitOrders2,limitPrices2=limitPrices2))
+  
+   # return(list(store=store,marketOrders=allzero,
+   #             limitOrders1=limitOrders1,limitPrices1=allzero,
+   #             limitOrders2=allzero,limitPrices2=allzero))
 }
 
 
 MomentumStrategy <- function(store, newRowList, currentPos, info, params, i, startIndex){ #params inside of the function.
+  #Things to condsider:
+    #Double Breakout?
+    #New Local Highs and Lows?
+  
+  #Using Market Orders
   position <- 0
   
   #Set Up
@@ -57,13 +75,11 @@ MomentumStrategy <- function(store, newRowList, currentPos, info, params, i, sta
   histogramValue.old <- (macd[macdlookback-2,1] - macd[macdlookback-2,2])   
   histogramValue.new <- (macd[macdlookback,1] - macd[macdlookback,2])   #MOMENTUM IS INCREASING #Most recent   (IF NEW > OLD = Acceleration in momentum) + if crossing above signal line (- if crossing below)
   
-  #MAYBE CONSIDER Double Breakout?
   if (cl < bbands[,"dn"]) {
      if (histogramValue.new < histogramValue.old && histogramValue.new < 0){ #Negative Divergence
        if (adx[,4] > 25) {  #VALUES (0-25:Weak : 25-50:Strong : 50-75:V.Strong : 75-100:Ext.Strong)                                    ##DETECT Sufficient STRENGTH to the breakout        //if very strong maybe give more funds, increase position?
          if (emv[,1] < -0.3) {  #VALUES (Break Above 0 is price rise with ease (below is price fall): Further from 0 equals stronger)             ##Avoid Fakeout, Is able to move more and in a positive
-           #go short (i.e., trend following)
-           position <- -params$posSizes[params$series[i]] - currentPos[[i]]
+           position <- -params$posSizes[params$series[i]] - currentPos[[i]] #Go short (trend following)
          }
        }
      }
@@ -72,14 +88,281 @@ MomentumStrategy <- function(store, newRowList, currentPos, info, params, i, sta
      if (histogramValue.new > histogramValue.old && histogramValue.new > 0){ #Positive Divergence
        if (adx[,4] > 25) {  #VALUES (0-25:Weak : 25-50:Strong : 50-75:V.Strong : 75-100:Ext.Strong)                                    ##DETECT Sufficient STRENGTH to the breakout
          if (emv[,1] > 0.3) {  #VALUES (Break Above 0 is price rise with ease (below is price fall): Further from 0 equals stronger)             ##Avoid Fakeout, Is able to move more and in a positive
-           #go long (again, trend following)
-           position <- params$posSizes[params$series[i]] - currentPos[[i]]
+           position <- params$posSizes[params$series[i]] - currentPos[[i]]  #Go long (trend following)
          }
        }
      }
   }
   return(position)
 }
+
+
+MeanRevStrategy <- function(store, newRowList, currentPos, info, params, i, startIndex){ #params inside of the function.
+  position <- 0
+  cl <- newRowList[[params$series[i]]]$Close
+  bbands <- last(BBands(store$cl[startIndex:store$iter,i],
+                        n=params$lookback,sd=params$sdParamShort))
+  #macdlookback  = 7
+  macd <- last(MACD(store$cl[startIndex:store$iter,i],nFast = params$nfast, nSlow = params$nSlow, nSig = params$nSig, percent = FALSE),2)
+  rsi <- last(RSI(store$cl[startIndex:store$iter,i],n=params$rsi))
+  rsi.2 <- last(RSI(store$cl[startIndex:store$iter,i],n=2))
+  #print(c(rsi,rsi.2))
+  ema <- last(EMA(store$cl[startIndex:store$iter,i],n=params$lookback, wilder = FALSE, ratio = NULL))
+  
+  HLCdf <- data.frame(High = store$hi[startIndex:store$iter,i],
+                      Low = store$low[startIndex:store$iter,i],
+                      Close = store$cl[startIndex:store$iter])
+  adx <- last(ADX(HLCdf,n=params$adx))
+  
+  
+  ema.20 <- last(EMA(store$cl[startIndex:store$iter,i],n=params$lookback, wilder = FALSE, ratio = NULL))
+  ema.50 <- last(EMA(store$cl[startIndex:store$iter,i],n=params$lookbackShort, wilder = FALSE, ratio = NULL))
+  #print(c(ema.20,ema.50))
+  sma.20 <- last(SMA(store$cl[startIndex:store$iter,i],n=params$lookback))
+  sma.50 <- last(SMA(store$cl[startIndex:store$iter,i],n=params$lookbackShort))
+  
+  obv <- last(OBV(store$cl[startIndex:store$iter,i],store$vol[startIndex:store$iter,i]))
+  #print(obv)
+  
+  #perChange <- ((sma.50-sma.20) / sma.50)
+  perChange <- (((sma.50-cl) / cl) * 100)
+  #print(perChange)
+  
+  
+  #RSI(2) Mean Reversion Strategy P/D = 1
+  ema.n <- last(EMA(store$cl[startIndex:store$iter,i],n=44, wilder = FALSE, ratio = NULL)) # 38 < x < 50
+
+  if (rsi.2 <= 10) {  #Oversold Condition could be a buy opportunity (Undervalued)
+    if (cl > ema.n) {   #Close is greater than ema (Mean reversion is possible)
+      if (adx[,4] < 25) { #Do not want strong upwards momentum #This is just weak momentum regardless
+        position <- params$posSizes[params$series[i]] - currentPos[[i]] #Go Long (Mean Reversion)
+      }
+    }
+  }
+  
+  if (rsi.2 <= 90) { #Overbought Condition could be a sell opportunity (Overvalued)
+    if (cl < ema.n) { #
+      if (adx[,4] < 25) {
+        position <- -params$posSizes[params$series[i]] - currentPos[[i]] 
+      }
+    }
+  }
+  
+  
+  # #RSI(2) Mean Reversion Strategy P/D = 1
+  # 
+  # ema.34 <- last(EMA(store$cl[startIndex:store$iter,i],n=44, wilder = FALSE, ratio = NULL)) # 38 < x < 50
+  # 
+  # # if (cl < bbands[,"dn"]) {   #Possible option for trend reversal
+  # #   if (macd[2,1] < 0){ #BELOW ZERO LINE (MAKES BULLISH MORE SIGNIFICANT)
+  # #     if (rsi <= 30) { 
+  # if (rsi.2 <= 10) {
+  #   if (cl > ema.34){
+  #     #       if ((macd[1,1] < macd[1,2]) && (macd[2,1] > macd[2,2])){ ##CROSS ABOVE SIGNAL LINE (BULLISH)
+  #     if (adx[,4] < 25) {    #WE DO NOT WANT STRONG MOMENTUM AGAINST UP (THIS IS JUST WEAK MOMENTUM REGARDLESS)
+  #       #           if(cl < sma.20) {
+  #       #             if(ema.20 < ema.50) {
+  #       #               #if close is relatively low go long (i.e., Mean Reversion)
+  #       #               #print("ping")
+  #       position <- params$posSizes[params$series[i]] - currentPos[[i]] 
+  #       #             }
+  #       #           }
+  #     }
+  #     #       }
+  #   }
+  # }
+  # #   }
+  # # }
+  # #} #else if (cl >= ema){
+  # # position <- 0
+  # #}
+  # 
+  # 
+  # 
+  # 
+  # # if (cl > bbands[,"up"]) {
+  # #   if (macd[2,1] < 0){ #BELOW ZERO LINE (MAKES BULLISH MORE SIGNIFICANT)
+  # #     if (rsi >= 70) { 
+  # if (rsi.2 <= 90) { 
+  #   if (cl < ema.34){
+  #     #       if(cl > sma.50) {
+  #     #         if ((macd[1,1] < macd[1,2]) && (macd[2,1] > macd[2,2])){ ##CROSS ABOVE SIGNAL LINE (BULLISH)
+  #     if (adx[,4] < 25) {
+  #       #             if(cl > sma.20) {
+  #       #               #if close is relatively low go long (i.e., Mean Reversion)
+  #       #               #print("pong")
+  #       position <- -params$posSizes[params$series[i]] - currentPos[[i]] 
+  #       #             }
+  #     }
+  #     #         }
+  #     #       }
+  #   }
+  # }
+  # # }
+  # 
+  # #} else if (cl <= ema){
+  # #position <- 0
+  # #}
+  
+  
+  
+  
+  # } else if (cl > bbands[,"up"]) {
+  #   if ((macd[1,1] > macd[1,2]) && (macd[2,1] < macd[2,2])){ ##CROSS BELOW
+  #     if (rsi > 70) { 
+  #       #if close is relatively low go long (i.e., Mean Reversion)
+  #       position <- -params$posSizes[params$series[i]] - currentPos[[i]] 
+  #     }
+  #   }
+  # }
+  return(position)
+}
+
+
+
+
+MeanRevStrategy2 <- function(store, newRowList, currentPos, info, params, i, startIndex){ #params inside of the function.
+  position <- 0
+  cl = store$cl[startIndex:store$iter]
+  bbands <- last(BBands(store$cl[startIndex:store$iter,i],
+                        n=params$lookback,sd=params$sdParam))
+  #print(bbands)
+  
+  ###Averages
+  smaShort <- last(SMA(store$cl[startIndex:store$iter,i],n=params$lookbackShort))
+  #print(smaShort)
+  sma <- last(SMA(store$cl[startIndex:store$iter,i],n=params$lookback))
+  #print(sma)
+  ema <- last(EMA(store$cl[startIndex:store$iter,i],n=params$lookback, wilder = FALSE, ratio = NULL))
+  #print(ema)
+  vwap <- last(VWAP(store$cl[startIndex:store$iter,i], store$vol[startIndex:store$iter,i], 1))
+  #print(vwap)
+  
+  macd <- last(MACD(store$cl[startIndex:store$iter,i],nFast = params$nfast, nSlow = params$nSlow, nSig = params$nSig, percent = FALSE),2)
+  #print(macd[,1])
+  
+  ### Extra Indicators
+  rsi <- last(RSI(store$cl[startIndex:store$iter,i],n=params$rsi))
+  
+  #ENTER THE MARKET
+  if (cl[i] < bbands[,"dn"]) {
+    # if close is relatively low go long (i.e., Mean Reversion)
+    position <- params$posSizes[params$series[i]] - currentPos[[i]] 
+  }
+  
+  if (macd[2,1] < 0){ #BELOW ZERO LINE (MAKES BULLISH MORE SIGNIFICANT)
+    if ((macd[1,1] < macd[1,2]) && (macd[2,1] > macd[2,2])){ ##CROSS ABOVE SIGNAL LINE (BULLISH)
+      position <- params$posSizes[params$series[i]] - currentPos[[i]]
+    }
+  }
+  
+  if (sma < ema)  {
+    position <- params$posSizes[params$series[i]] - currentPos[[i]]
+  }
+  
+  if (rsi <= 30){
+    position <- params$posSizes[params$series[i]] - currentPos[[i]]
+  }
+  
+  if (vwap < bbands[,"dn"]) {
+    position <- params$posSizes[params$series[i]] - currentPos[[i]]
+  }
+  
+  
+  
+  
+  #EXIT THE MARKET
+  if (cl[i] > bbands[,"up"]) {
+    # if close is relatively high go short (again, Mean Reversion)
+    position <- -params$posSizes[params$series[i]] - currentPos[[i]]
+  }
+  
+  
+  if ((macd[1,1] > macd[1,2]) && (macd[2,1] < macd[2,2])){ ##CROSS BELOW
+    position <- -params$posSizes[params$series[i]] - currentPos[[i]]
+  }
+  
+  #if (rsi >= 65){
+  #  pos[params$series[i]] <- -params$posSizes[params$series[i]]
+  #}
+  
+  
+  #MORE SPECIFIC (COMBINE INDICATORS)
+  #if (cl[i] < bbands[,"dn"]) {
+  #  if (macd[2,1] < 0)  {
+  #    if ((macd[1,1] < macd[1,2]) && (macd[2,1] > macd[2,2])){ ##CROSS ABOVE SIGNAL LINE (BULLISH)
+  #      pos[params$series[i]] <- params$posSizes[params$series[i]]
+  #    }
+  #  }
+  #}
+  
+  return(position)
+}
+
+MarketMakingStrategy <- function(store, newRowList, currentPos, info, params, i, startIndex){ #params inside of the function.
+  position <- 0
+  #cat("currentPos", formatC(currentPos,3),"\n")
+  
+  # check if current inventory is above a limit and if so exit completely
+  # with a market order
+  marketOrders <- ifelse(abs(currentPos) > params$inventoryLimits, -currentPos, 0)      #Security Measure
+  
+  
+  
+  
+  
+  # use the range (High-Low) as a indicator for a reasonable "spread" for
+  # this pseudo market making strategy
+  spread <- sapply(1:length(newRowList),function(i)
+    params$spreadPercentage * (newRowList[[i]]$High -
+                                 newRowList[[i]]$Low))
+  
+
+  macd <- last(MACD(store$cl[startIndex:store$iter,i],nFast = params$nfast, nSlow = params$nSlow, nSig = params$nSig, percent = FALSE),2)
+  
+  if ((macd[1,1] < macd[1,2]) && (macd[2,1] > macd[2,2])){ ##CROSS ABOVE SIGNAL LINE (BULLISH)
+    #we expect to see a price rise (opposite for bullish and price fall)
+  }
+  
+  rsi <- last(RSI(store$cl[startIndex:store$iter,i],n=params$rsi))
+  print(rsi)
+  
+  if(rsi < 30){   #Stock oversold is undervalued -  so we expect the price to go up
+    
+  }
+  
+  
+  
+  if (rsi > 70){  #Stock overbought is overvalued -  so we expect the price to go down
+    
+  }
+  
+  obv <- last(OBV(store$cl[startIndex:store$iter,i],store$vol[startIndex:store$iter,i]))
+  #print(obv)
+  
+  cl <- newRowList[[params$series[i]]]$Close
+  ema <- last(EMA(store$cl[startIndex:store$iter,i],n=params$lookback, wilder = FALSE, ratio = NULL))
+  #if(cl < ema) ()
+  
+  
+  limitOrders1  <- rep(1,length(newRowList)) # BUY LIMIT ORDERS
+  limitPrices1  <- sapply(1:length(newRowList),function(i)
+    newRowList[[i]]$Close - spread[i]/2)
+
+  limitOrders2  <- rep(-1,length(newRowList)) # SELL LIMIT ORDERS
+  limitPrices2  <- sapply(1:length(newRowList),function(i)
+    newRowList[[i]]$Close + spread[i]/2)
+  
+  
+  # 29216 37066
+  return(list(store=store,marketOrders=marketOrders,
+              limitOrders1=limitOrders1,
+              limitPrices1=limitPrices1,
+              limitOrders2=limitOrders2,
+              limitPrices2=limitPrices2))
+
+}
+
 
 
   ### Store Functions ###
