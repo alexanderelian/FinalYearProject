5e75746eee47510cfd4e6b74c8cec97cc6000949
Alexander Elian <alex.elian14@outlook.com>
2021-03-30 01:33:02 +0100

    Corrected Fitness Plotter Code

--- main_optimize.R
+++ main_optimize.R
@@ -6,7 +6,7 @@ source('strategies/strat.R')
 
 
 tmp <<- matrix(ncol= 0, nrow = 22)
-print("tmp matrix created")
+print("tmp matrix created in main_optimize")
 
 
 currentMax = 0
@@ -18,7 +18,7 @@ dataList <- lapply(dataList, function(x) x[1:numOfDays])
 sMult <- 0.2 # slippage multiplier
 
 #adxSeq <- seq(from=15,to=65,by=5)
-adxSeq <- seq(from=25,to=45,by=5)
+adxSeq <- seq(from=20,to=40,by=5)
 emvSeq  <- seq(from=0,to=1,by=0.2) 
 sdSeq <- seq(from=1, to=2, by=0.5)
 holdingLong <- seq(from=1, to=21, by=1)
@@ -26,40 +26,48 @@ paramsList  <- list(adxSeq,sdSeq)
 numberComb <- prod(sapply(paramsList,length))
 
 resultsMatrix <- matrix(nrow=(length(adxSeq)),ncol=length(sdSeq))
+holdingDaysMatrix <- matrix(nrow=(length(adxSeq)),ncol=length(sdSeq))
+positionTakenMatrix <- matrix(nrow=(length(adxSeq)),ncol=length(sdSeq))
+ROIResults <- matrix(nrow=(length(adxSeq)),ncol=length(sdSeq))
+
 #colnames(resultsMatrix) <- c("0","0.1","0.2","0.3","0.4","0.5")
 pfolioPnLList <- vector(mode="list",length=numberComb)
 count <- 1
+
 for (x in 1:length(adxSeq)) {   #loop through first param
-    print(x)
     for (i in 1:length(sdSeq)) {    #loop through first param
-            print(i)
         
-            tmp <<- matrix(ncol= 0, nrow = 22)  #aggreagte ROI
-
-            # #params <- list(lookback=lb,sdParam=sdp,series=1:4,posSizes=rep(1,10))
-            params <- list(lookback=50,lookbackShort=34,sdParam=sdSeq[i],sdParamShort=1,adx=14,adxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=c(0,0,0,0,0,0,0,0,0,0),shortHoldPeriod=c(0,0,0,0,0,0,0,0,0,0),series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
-            # #params <- list(lookback=50,lookbackShort=34,sdParam=sdSeq[i],sdParamShort=1,adx=14,adxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=holdingLong[days],shortHoldPeriod=c(0,0,0,0,0,0,0,0,0,0),series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
-            print(c("param sent is" ,params$sdParam[i]))
+            tmp <<- matrix(ncol= 0, nrow = 22)  #aggreagte ROI for every holding day
+                #Hold Periods do not matter, as we enter then plot the next 21 days...
+            params <- list(lookback=50,lookbackShort=34,MOsdParam=sdSeq[i],sdParamShort=1,adx=14,MOadxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=c(0,0,0,0,0,0,0,0,0,0),shortHoldPeriod=c(0,0,0,0,0,0,0,0,0,0),series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
             results <- backtest(dataList, getOrders, params, sMult)
-
-            print(tmp)
+            
             avg <- matrix(ncol= 1, nrow = 22)
             for (a in 1:22){
                 avg[a,1]<-(mean(tmp[a,]))    #get avg
             }
             avgExitDay <- which(avg == max(avg), arr.ind = TRUE)
-            # #print(avg)
-            print(paste("Exit at ", max(avg)," Which is day ", avgExitDay[1,1]-1,sep = ""))     #min of avg for shorting, as looking for a price drop, not price increase
-            #
-            if(currentMax < max(avg))   {bestHolding = avgExitDay[1,1]-1}
+ 
+            print(paste("Exit at ", max(avg)," Which is day ", avgExitDay[1,1]-1,sep = ""))  
+            BH <- as.integer(avgExitDay[1,1]-1) #minus one as the day 1 is 0 days.
             
-        
-            BH <- bestHolding
-
-            params <- list(lookback=50,lookbackShort=34,sdParam=sdSeq[i],sdParamShort=1,adx=14,adxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=c(0,0,0,0,0,0,0,0,0,0),shortHoldPeriod=c(BH,BH,BH,BH,BH,BH,BH,BH,BH,BH),series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
+            print("plotting with params:")
+            print(c("adx : ", adxSeq[x]))
+            print(c("sd : ", sdSeq[i]))
+            print(c("Holding : ", BH))
+            
+            #First for short, as holding for long is 0, and holding for every short is the optimal BH
+            #Vise versa for the second line...
+            #params <- list(lookback=50,lookbackShort=34,MOsdParam=sdSeq[i],sdParamShort=1,adx=14,MOadxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=0,shortHoldPeriod=c(BH,BH,BH,BH,BH,BH,BH,BH,BH,BH),series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
+            params <- list(lookback=50,lookbackShort=34,MOsdParam=sdSeq[i],sdParamShort=1,adx=14,MOadxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=c(BH,BH,BH,BH,BH,BH,BH,BH,BH,BH),shortHoldPeriod=0,series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
             #params <- list(lookback=50,lookbackShort=34,sdParam=sdSeq[i],sdParamShort=1,adx=14,adxSignal=adxSeq[x],emvLong=0.3,emvShort=-0.3,nfast=12,nSlow=26,nSig = 9,rsi=14,holdPeriod=holdingLong[days],shortHoldPeriod=c(0,0,0,0,0,0,0,0,0,0),series=c(1,2,3,4,5,6,7,8,9,10),posSizes=rep(100,10),spreadPercentage=0.001,inventoryLimits=rep(10,10))
-            #source('main.R')
             results <- backtest(dataList, getOrders, params, sMult)
+            
+            #print(names(results))
+            holdingDaysMatrix[x,i] <- BH
+            positionTakenMatrix[x,i] <- results$k
+            ROIResults[x,i] <- max(avg)
+            
             
             pfolioPnL <- plotResults(dataList,results)
             #resultsMatrix[count,] <- c(ad,em,pfolioPnL$fitAgg)
@@ -75,23 +83,32 @@ for (x in 1:length(adxSeq)) {   #loop through first param
             
     }
 }
-print(resultsMatrix)
-#print(length(resultsMatrix[order(resultsMatrix[,"PD Ratio"]),]))
+print("Optimal Holding Periods (ROI)")
+print(holdingDaysMatrix)
+print("Positions Taken")
+print(positionTakenMatrix)
 
-# print(test)
-# 
-# test[1,1] <- resultsMatrix[1,3]
-# test[1,2] <- resultsMatrix[2,3]
-# test[2,1] <- resultsMatrix[3,3]
-# test[2,2] <- resultsMatrix[4,3]
-# test[3,1] <- resultsMatrix[5,3]
-# test[3,2] <- resultsMatrix[6,3]
-# print(test)
-# 
+
+
+print("PD Matrix")
+print(resultsMatrix)
 library(reshape2)
 p <- ggplot(melt(resultsMatrix),aes(x=Var1, y=Var2, fill=value))
+p <- p + labs(title= "PD Fitness")
 p <- p + scale_y_discrete(name="standard deviation", limits = c("1","1.5","2"))
-p <- p + scale_x_discrete(name="adx", limits = c("20","25","30","35","40","45"))
+p <- p + scale_x_discrete(name="adx", limits = c("20","25","30","35","40"))
 p <- p + geom_tile()
-plot(p)
-print(resultsMatrix)
+#plot(p)
+
+
+print("ROI Matrix")
+print(ROIResults)
+q <- ggplot(melt(ROIResults),aes(x=Var1, y=Var2, fill=value))
+q <- q + labs(title= "ROI Fitness")
+q <- q + scale_y_discrete(name="standard deviation", limits = c("1","1.5","2"))
+q <- q + scale_x_discrete(name="adx", limits = c("20","25","30","35","40"))
+q <- q + geom_tile()
+#plot(q)
+
+grid.arrange(p,q)
+
